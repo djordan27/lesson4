e9d8cfcb7029c50e6540642141143e1bfb084118
Игорь <211353@mail.ru>
2023-01-29 17:49:25 +0300

    finish

---
+++ task2.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+int GetNumber() {
+    int result = 0;
+    while(true) {
+        if(int.TryParse(Console.ReadLine(), out result)){
+            break;
+        } else {
+            Console.WriteLine("Вы ввели недопустимое значение введите число");
+        }
+    }
+    return result;
+}
+
+void sum() {
+    int number = Math.Abs(GetNumber());
+    int sumNumber = 0;
+    int divider = 10;
+    int divider1 = 1;
+    while(number / divider > 0){
+        sumNumber = sumNumber + (number % divider) / divider1;
+        divider = divider * 10;
+        divider1 = divider1 *10;
+    }
+    sumNumber = sumNumber + number / divider1;
+    Console.WriteLine($"{number} -> {sumNumber}");
+}
+
+sum();
---
+++ task3.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+string GetNumber()
+{
+    string result = Console.ReadLine();
+    return result;
+}
+//1.создание массива
+//2.проверка 1ого элемента 1 индекса массива, на переопределение Int
+//3.передача успешных элементов в 2ой массив
+string []finalArray(string number){
+    string [] firstArray = number.Split(',', ' ');
+    string [] lastArray = new string [number.Length];
+    string word;
+    int numberTry;
+    int countSuccess = 0;
+    int countLastArray = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        word = firstArray[i];
+        for (int j = 0; j < word.Length; j++)
+        {
+            bool success = int.TryParse(word.Substring(j, 1), out numberTry);
+            if (success) {
+                lastArray[countLastArray] = lastArray[countLastArray] + word.Substring(j, 1);
+                countSuccess++;
+            }
+        }
+        
+        if(countSuccess > 0) {
+            countLastArray++;
+            countSuccess = 0;
+        }
+    }
+     return lastArray;
+}
+
+int[] arrayOfNumber(string[] array)
+{
+    int[] arrayOfNumber = new int[array.Length];
+    int countArray = 0;
+    int countArrayOfNumber = 0;
+    while (countArray < array.Length)
+    {
+        if (int.TryParse(array[countArray], out arrayOfNumber[countArrayOfNumber]))
+        {
+            countArrayOfNumber++;
+        }
+        countArray++;
+    }
+    Array.Resize(ref arrayOfNumber, countArrayOfNumber);
+    return arrayOfNumber;
+}
+string number = GetNumber();
+string[] array = finalArray(number);
+int[] arrayNumber = arrayOfNumber(array);
+Console.Write("[");
+for (int i = 0; i < arrayNumber.Length - 1; i++)
+{
+    Console.Write($"{arrayNumber[i]},");
+}
+Console.Write($"{arrayNumber[arrayNumber.Length - 1]}]");
+
